8e3433b57a206ec880183d99970a91d6df3537e6
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-08-24 14:26:46 +0000

    Modified anagram.rb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word, :expression
+    def initialize(word)
+        @word = word
+    end
+
+    def match(expression)
+        @expression = expression
+        @expression.filter do |wrd|
+            wrd = wrd.chars
+            @word.chars.permutation.to_a.include?(wrd) 
+        end
+    end 
+end
